53b58d90c0d55ee499973245cdd319b362910c08
jeffg2221 <jeffg2221@icloud.com>
2025-04-13 22:17:13 -0400

    sample types

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Jeff";
+var lastName = "Gomez";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
